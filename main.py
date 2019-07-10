4dbfa4f762791257f5b3e04a5bcdd3a088ab8734
sarimaleem <34404894+sarimaleem@users.noreply.github.com>
2019-07-10 14:47:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,12 @@ from ExtractPupil import create_files
 import time
 
 start = time.time()
-
 directory = 'C:/Users/Sarim Aleem/Documents/PupilProject/data/cropped'
 
 folders = [x[0] for x in os.walk(directory)][1:]
-#create_files(folders[0])
+#create_files(folders[0]) #if you only want to do one of the directories
+
+Parallel(n_jobs=48)(delayed(create_files)(path) for path in folders)
 
 end = time.time()
-
 print(end - start)
-
-#Parallel(n_jobs=8)(delayed(create_files)(path) for path in folders)
